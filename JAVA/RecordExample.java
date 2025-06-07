b0542491ae121a9bdce810f843e538d222d0f72e
Arpita Mishra <139300220+Arps09@users.noreply.github.com>
2025-06-07 09:46:31 +0530

    Delete JAVA directory

--- JAVA/RecordExample.java
+++
@@ -1,7 +0,0 @@
-record Person(String name, int age) {}
-public class RecordExample {
-    public static void main(String[] args) {
-        Person p = new Person("Alice", 22);
-        System.out.println(p.name() + ", " + p.age());
-    }
-}
